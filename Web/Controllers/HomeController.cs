18076df733d78e2f62c03884f23a29529dfde4c0
pilo4560 <octaviopm@live.com>
2021-06-15 12:46:22 -0400

    Web2

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Students()
+        {
+            List<StudentModel> students = AllStudents();
+            return View(students);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        List<StudentModel> AllStudents()
+        {
+            string connString = "Server=OCTAVIO-LAPTOP;Database=INTEC;User Id=intec;Password=intec;";
+            using (SqlConnection sqlConnection = new SqlConnection(connString))
+            {
+                SqlCommand sqlCommand = new SqlCommand("sp_student", sqlConnection);
+                sqlConnection.Open();
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                List<StudentModel> studentModels = new List<StudentModel>();
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    StudentModel studentModel = new StudentModel();
+                    studentModel.Id = new Guid(dt.Rows[i]["Id"].ToString());
+                    studentModel.Seq = Convert.ToInt32(dt.Rows[i]["Seq"]);
+                    studentModel.FirstName = dt.Rows[i]["FirstName"].ToString();
+                    studentModel.LastName = dt.Rows[i]["LastName"].ToString();
+                    studentModel.NickName = dt.Rows[i]["Nickname"].ToString();
+                    studentModel.DayOfBirth = Convert.ToDateTime(dt.Rows[i]["DayOfBirth"].ToString());
+                    studentModel.Email = dt.Rows[i]["Email"].ToString();
+                    studentModel.PhoneNumber = dt.Rows[i]["PhoneNumber"].ToString();
+                    studentModel.CreatedDate = Convert.ToDateTime(dt.Rows[i]["CreatedDate"].ToString());
+
+                    studentModels.Add(studentModel);
+                }
+
+               return studentModels;
+            }
+        }
+    }
+}
---
+++ Web/Controllers/StudentsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web;
+
+namespace Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private INTECEntities db = new INTECEntities();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(db.Student.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                student.Id = Guid.NewGuid();
+                student.CreatedDate = DateTime.Now;
+                db.Student.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Seq,FirstName,LastName,NickName,DayOfBirth,Email,PhoneNumber,CreatedDate")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Student.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Student student = db.Student.Find(id);
+            db.Student.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
